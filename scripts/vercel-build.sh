3c0926a4d91deb751502fed1cfe783440b03d4cf
shrixtacy <shriyanshdash12@gmail.com>
2026-03-02 15:01:56 +0530

    fix(vercel): move long build command to script

---
+++ scripts/vercel-build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+./flutter/bin/flutter config --enable-web
+./flutter/bin/flutter pub get
+./flutter/bin/flutter build web --release --web-renderer canvaskit \
+  --dart-define=ENABLE_BACKEND="${ENABLE_BACKEND:-false}" \
+  --dart-define=FIREBASE_API_KEY="${FIREBASE_API_KEY:-}" \
+  --dart-define=FIREBASE_APP_ID="${FIREBASE_APP_ID:-}" \
+  --dart-define=FIREBASE_MESSAGING_SENDER_ID="${FIREBASE_MESSAGING_SENDER_ID:-}" \
+  --dart-define=FIREBASE_PROJECT_ID="${FIREBASE_PROJECT_ID:-}" \
+  --dart-define=FIREBASE_AUTH_DOMAIN="${FIREBASE_AUTH_DOMAIN:-}" \
+  --dart-define=FIREBASE_STORAGE_BUCKET="${FIREBASE_STORAGE_BUCKET:-}" \
+  --dart-define=FIREBASE_MEASUREMENT_ID="${FIREBASE_MEASUREMENT_ID:-}" \
+  --dart-define=GOOGLE_MAPS_API_KEY="${GOOGLE_MAPS_API_KEY:-}"
